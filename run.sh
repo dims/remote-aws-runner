65fb0423a6e9944d66d6070e391d841b11cc0cec
Davanum Srinivas <davanum@gmail.com>
2023-04-06 09:15:39 -0400

    first round of patch to get the scripts working
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
-###
-
-```bash
 DELETE_INSTANCES=true \
   FOCUS=NodeConformance \
   IMAGE_CONFIG_FILE=aws-instance.yaml \
@@ -8,4 +5,3 @@ DELETE_INSTANCES=true \
   TEST_ARGS='--container-runtime-endpoint=unix:///run/containerd/containerd.sock --container-runtime-process-name=/usr/bin/containerd --container-runtime-pid-file= --kubelet-flags="--cgroups-per-qos=true --cgroup-root=/ --runtime-cgroups=/system.slice/containerd.service" --extra-log="{\"name\": \"containerd.log\", \"journalctl\": [\"-u\", \"containerd*\"]}"' \
   SSH_USER=ec2-user \
   hack/make-rules/test-e2e-node.sh
-```
